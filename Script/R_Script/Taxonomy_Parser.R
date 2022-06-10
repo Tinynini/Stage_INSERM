36b3f4b6caa2b2670ef0d5932eb390245b4644d5
Tinynini <66923814+Tinynini@users.noreply.github.com>
2022-06-10 16:49:48 +0200

    Update Taxonomy_Parser.R

--- Script/R_Script/Taxonomy_Parser.R
+++ Script/R_Script/Taxonomy_Parser.R
@@ -1,18 +1,18 @@
 library(tidyverse)
 
-#### Ouverture de bac120_taxonomy_r95.tsv (ou de bac120_taxonomy_r95_new.tsv pour la version alternative) & récupération des données ####
+#### Ouverture de bac120_taxonomy_r95.tsv (ou de bac120_taxonomy_r95_new.tsv pour la 2nd version) & rÃ©cupÃ©ration des donnÃ©es ####
 taxonomy <- read.csv('W:/ninon-species/data/bac120_taxonomy_r95.tsv', sep = ';', header = FALSE)
 #taxonomy <- read.csv('W:/ninon-species/data/bac120_taxonomy_r95_new.tsv', sep = ';', header = FALSE) 
 colnames(taxonomy) <- c('Occurrence', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species')
 #colnames(taxonomy) <- c('sseqid', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species')
 
-#### Modification de la nomenclature de la table de taxonomie en vue de son join avec nos propre données ####
-for (i in 2:ncol(taxonomy)) # Suppression des préfix indiquant le niveau taxonomique (superflus car déjà indiqué par les nom de colonnes)
+#### Modification de la nomenclature de la table de taxonomie en vue de son join avec nos propre donnÃ©es ####
+for (i in 2:ncol(taxonomy)) # Suppression des prÃ©fix indiquant le niveau taxonomique (superflus car dÃ©jÃ  indiquÃ©s par les nom de colonnes)
 {
   taxonomy[,i] <- str_replace(taxonomy[,i], pattern = '(...)(.*)', replacement = "\\2")
 }
 
-for (j in 1:nrow(taxonomy)) # Suppression des suppléments de matricule en suffix chez les espèces 'sp' (également superflus)
+for (j in 1:nrow(taxonomy)) # Suppression des supplÃ©ments de matricule en suffix chez les espÃ¨ces 'sp' (Ã©galement superflus)
 {
   if (str_replace(taxonomy[j,'Species'], pattern = "(.*) (..)(.*)", replacement = "\\2") == 'sp')
   {
@@ -24,11 +24,11 @@ taxonomy %>%
   arrange(Species) %>% 
   identity() -> taxonomy
 
-taxonomy <- taxonomy[,-c(1)] # Pour la 1ère version uniquement
+taxonomy <- taxonomy[,-c(1)] # Pour la 1Ã¨re version uniquement
 taxonomy <- rev(unique(taxonomy))
 
-#### Elimination de certaines sous-classifications propres à l'étude dont est tirée cette table (et parasites pour la notre !) ####
-for (k in 2:(ncol(taxonomy) - 1)) # On commence à 2 car la colonne des espèces n'a pas besoin d'être traitée 
+#### Elimination de certaines sous-classifications propres Ã  l'Ã©tude dont est tirÃ©e cette table (et parasites pour la notre !) ####
+for (k in 2:(ncol(taxonomy) - 1)) # On commence Ã  2 car la colonne des espÃ¨ces n'a pas besoin d'Ãªtre traitÃ©e 
 {
   sub <- grep('(.*)_(.*)', taxonomy[, k])
   taxonomy[sub, k] <- str_replace(taxonomy[sub, k], '(.*)_(.*)', '\\1')
@@ -40,9 +40,9 @@ taxonomy[sub_espece1, 1] <- str_replace(taxonomy[sub_espece1, 1], '(.*)_(.*) (.*
 sub_espece2 <- grep('(.*) (.*)_(.*)', taxonomy[, 1])
 taxonomy[sub_espece2, 1] <- str_replace(taxonomy[sub_espece2, 1], '(.*) (.*)_(.*)', '\\1\\ \\2')
 
-#taxonomy <- rev(unique(taxonomy)) # Pour la version alternative uniquement
+#taxonomy <- rev(unique(taxonomy)) # Pour la 2nd version uniquement
 
-#### Traitement préventif des données pour éviter la création de certains doublons lors du join ####
+#### Traitement prÃ©ventif des donnÃ©es pour Ã©viter la crÃ©ation de certains doublons lors du join ####
 sub2 <- grep('Bacillus', taxonomy[, 'Genus'])
 taxonomy[sub2,'Family'] <- 'Bacillaceae'
 taxonomy[sub2, 'Order'] <- 'Bacillales'
@@ -61,6 +61,6 @@ taxonomy[sub5, 'Order'] <- 'Lachnospirales'
 
 taxonomy <- unique(taxonomy)
 
-#### Enregistrement de la table de taxonomie dans le fichier Parsed_taxonomy.tsv (ou dans le fichier New_Parsed_taxonomy.tsv pour la version alternative) ####
+#### Enregistrement de la table de taxonomie dans le fichier Parsed_taxonomy.tsv (ou dans le fichier New_Parsed_taxonomy.tsv pour la 2nd version) ####
 write.table(taxonomy, "W:/ninon-species/output/Parsed_taxonomy.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
 #write.table(taxonomy, "W:/ninon-species/output/New_Parsed_taxonomy.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
