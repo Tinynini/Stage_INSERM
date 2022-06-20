babdbe093431dfaadee622e9390e66daffc7d9a2
Tinynini <66923814+Tinynini@users.noreply.github.com>
2022-06-20 14:46:03 +0200

    Update Species_Matrix_Parser.R

--- Script/R_Script/Species_Matrix_Parser.R
+++ Script/R_Script/Species_Matrix_Parser.R
@@ -14,13 +14,13 @@ gene <- sort(str_replace(unique(all_species$qseqid), pattern = '(.*)_(.*)_(.*)',
 rownames(matrix) <- gene
 
 #### Exemple des barplots d'absence/présence que l'on obtient pour un ARG de la famille des 'aph' (aph(3')-XV) ####
-barplot(matrix[257,], main = "Partage inter-espèces de aph(3')-XV", axisnames = FALSE)
+barplot(matrix[258,], main = "Partage inter-espèces de aph(3')-XV", axisnames = FALSE)
 #barplot(matrix[121,], main = "Partage inter-espèces de aph(3')-XV") # Version réduite
 
 # Là, on ne conserve que les présences pour pouvoir voir les nom d'espèces se partageant l'ARG (avec le zoom en plein écran ou en étirant suffisement la zone de plot)
-to_set <- which(matrix[257,] != 0)
+to_set <- which(matrix[258,] != 0)
 #to_set <- which(matrix[121,] != 0) # Version réduite
-m <- matrix[257, c(to_set)]
+m <- matrix[258, c(to_set)]
 #m <- matrix[121, c(to_set)] # Version réduite
 barplot(m, main = "Partage inter-espèces de aph(3')-XV")
 
