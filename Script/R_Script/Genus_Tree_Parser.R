e881af2b5b0e07ede84043002e57bfb2def9fc28
Tinynini <66923814+Tinynini@users.noreply.github.com>
2022-06-21 18:22:52 +0200

    Update Genus_Tree_Parser.R

--- Script/R_Script/Genus_Tree_Parser.R
+++ Script/R_Script/Genus_Tree_Parser.R
@@ -71,11 +71,11 @@ n_centro <- nrow(uni_centro)
 
 #### Plot de l'histogramme des distances ####
 genus_length <- uni_centro['length']
-splot <- ggplot(genus_length, aes(length)) + geom_histogram(bins = n_centro)
-splot + ggtitle("Distance inter-genus")
+gplot <- ggplot(genus_length, aes(length)) + geom_histogram(bins = n_centro)
+gplot + ggtitle("Nombres d'occurrences des valeurs de distances inter-genres") + xlab("valeurs des distances") + ylab("Nombres d'occurrences")
 
-#### Exemple de plot d'un sous_arbre avec le 2eme centroid de la liste ####
-plot.phylo(tree_list[[2]], show.node.label = TRUE, main = uni_centro[2, 1], sub = uni_centro[2, 2])
+#### Exemple de plot d'un sous_arbre avec "mef(B)_1_FJ196385" ####
+plot.phylo(tree_list[[1228]], show.node.label = TRUE, main = uni_centro[1228, 1], sub = uni_centro[1228, 2])
 
 #### Plot des sous-arbres des génus par centroides sur l'arbre complet ####
 liste <- vector(mode = 'list', length = n_centro)
