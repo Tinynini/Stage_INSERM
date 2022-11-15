574faad77396ab32a6e71641e1b36eeaf6c962ab
Tinynini <66923814+Tinynini@users.noreply.github.com>
2022-11-15 11:23:18 +0100

    Add files via upload

---
+++ Script/Improved_R_Script/Matrix_Binaire.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+
+all_species <- read_tsv('W:/ninon-species/output/Total_ARG_Species.tsv') %>% 
+  #all_species <- read_tsv('W:/ninon-species/output/Sliced_All_Species_clust.tsv') %>% 
+  as.data.frame()
+
+level <- as.data.frame(all_species[, c(7:12)])
+level_name <- unlist(colnames(all_species[, c(7:12)]))
+
+uni_centro <- sort(unique(all_species$Centroid))
+n_centro <- length(uni_centro)
+
+for (i in 1:6)
+{
+  uni_level <- as.data.frame(sort(unique(level[, i])))
+  colnames(uni_level) <- level_name[i]
+  n_level <- nrow(uni_level)
+
+  centro_matrix <- matrix(data = 0, nrow = n_centro, ncol = n_level)
+  rownames(centro_matrix) <- uni_centro
+  colnames(centro_matrix) <- uni_level[, 1]
+  
+  all_species %>% 
+    select(Centroid, level_name[i]) %>% 
+    identity() -> arg_level
+   
+  for (j in 1:n_centro)
+  {
+    curr_centro <- uni_centro[j]
+    curr_level <- arg_level[arg_level$Centroid == curr_centro, level_name[i]]
+    
+    to_set <- which(uni_level[, level_name[i]] %in% curr_level)
+    centro_matrix[j, to_set] <- 1                       
+  }
+  
+  path_start = "W:/ninon-species/output/Matrix_"
+  #path_start = "W:/ninon-species/output/Sliced_Matrix_"
+  path_end = ".tsv"
+  file_name = str_glue("{path_start}{level_name[i]}{path_end}")
+
+  write.table(centro_matrix, file_name, sep = '\t', row.names = FALSE, col.names = TRUE)
+}
---
+++ Script/Improved_R_Script/Matrix_Pseudo_Binaire.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+
+all_species <- read_tsv('W:/ninon-species/output/Total_ARG_Species.tsv') %>% 
+  #all_species <- read_tsv('W:/ninon-species/output/Sliced_All_Species_clust.tsv') %>% 
+  as.data.frame()
+
+level <- as.data.frame(all_species[, c(7:12)])
+level_name <- unlist(colnames(all_species[, c(7:12)]))
+
+uni_centro <- sort(unique(all_species$Centroid))
+n_centro <- length(uni_centro)
+
+for (i in 1:5)
+{
+  path_start = "W:/ninon-species/output/Matrix_"
+  #path_start = "W:/ninon-species/output/Sliced_Matrix_"
+  path_end = ".tsv"
+  file_name = str_glue("{path_start}{level_name[i]}{path_end}")
+  
+  now_level <- as.data.frame(sort(unique(level[, i]))) 
+  colnames(now_level) <- level_name[i]
+  
+  for (j in (i + 1):6)
+  {
+    uni_level <- unlist(as.data.frame(sort(unique(level[, j]))))
+    n_level <- length(uni_level)
+
+    curr_level <- as.data.frame(unique(level[, c(j, i)]))
+    na_level <- which(is.na(curr_level[, 1]) == TRUE)
+    curr_level <- curr_level[-c(na_level),]
+    
+    curr_level %>%
+      arrange_(level_name[j]) %>%
+      identity() -> curr_level
+    
+    centro_matrix <- read_tsv(file_name) ## Pourquoi la transposition bug ????
+    rownames(centro_matrix) <- uni_centro
+    centro_matrix <- t(centro_matrix)
+    centro_matrix <- cbind(now_level, centro_matrix)
+    centro_matrix <- left_join(curr_level, centro_matrix, by = NULL)
+    centro_matrix <- t(centro_matrix[-2])
+    colnames(centro_matrix) <- centro_matrix[1,]
+    centro_matrix <- as.data.frame(centro_matrix[-1,])
+
+    for (n in 1:ncol(centro_matrix))
+    {
+      centro_matrix[, n] <- as.integer(c(centro_matrix[, n]))
+    }
+
+    centro_matrix <- as.matrix(centro_matrix)
+    
+    cross_matrix <- matrix(data = 0, nrow = n_centro, ncol = n_level)
+    colnames(cross_matrix) <- uni_level
+    rownames(cross_matrix) <- uni_centro
+
+    for (k in 1:n_level)
+    {
+      to_set <- which(curr_level[, 1] %in% uni_level[k])
+      l <- length(to_set)
+      m <- centro_matrix[, c(to_set)]
+
+      if (l > 1)
+      {
+        cross_matrix[, k] <- rowSums(m)
+      }
+
+      else
+      {
+        cross_matrix[, k] <- m
+      }
+    }
+
+    path_start = "W:/ninon-species/output/Matrix_"
+    #path_start = "W:/ninon-species/output/Sliced_Matrix_"
+    path_end = ".tsv"
+    new_file_name = str_glue("{path_start}{level_name[i]}_{level_name[j]}{path_end}")
+
+    write.table(cross_matrix, new_file_name, sep = '\t', row.names = FALSE, col.names = TRUE)
+  }
+}
