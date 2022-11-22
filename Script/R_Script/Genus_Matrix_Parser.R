7e4fdcaee245de86685771ef259428cc6273906e
Tinynini <66923814+Tinynini@users.noreply.github.com>
2022-11-22 15:00:32 +0100

    Update Genus_Matrix_Parser.R

--- Script/R_Script/Genus_Matrix_Parser.R
+++ Script/R_Script/Genus_Matrix_Parser.R
@@ -10,19 +10,19 @@ all_species <- read_tsv('W:/ninon-species/output/all_species_clust.tsv') %>%
 
 Genus_matrix <- as.matrix(Genus_matrix)
 
-gene <- sort(str_replace(unique(all_species$qseqid), pattern = '(.*)_(.*)_(.*)', replacement = "\\1"))
+gene <- sort(unique(all_species$qseqid))
 rownames(Genus_matrix) <- gene
 
 #### Exemple des barplots d'absence/présence que l'on obtient pour un ARG de la famille des 'aph' (aph(3')-XV) ####
-barplot(Genus_matrix[258,], main = "Partage inter-génus de aph(3')-XV", axisnames = FALSE) 
-#barplot(Genus_matrix[121,], main = "Partage inter-génus de aph(3')-XV") # Version réduite 
+barplot(Genus_matrix[258,], main = "Partage inter-génus de aph(3')", axisnames = FALSE) 
+#barplot(Genus_matrix[121,], main = "Partage inter-génus de aph(3')") # Version réduite 
 
 # Là, on ne conserve que les présences pour pouvoir voir les nom de génus se partageant l'ARG (avec le zoom en plein écran ou en étirant suffisement la zone de plot)
 to_set <- which(Genus_matrix[258,] != 0)
 #to_set <- which(Genus_matrix[121,] != 0) # Version réduite
 m <- Genus_matrix[258, c(to_set)]
 #m <- Genus_matrix[121, c(to_set)] # Version réduite
-barplot(m, main = "Partage inter-génus de aph(3')-XV")
+barplot(m, main = "Partage inter-génus de aph(3')")
 
 #### Exemple de calcul des distances et du plot du dendrogramme associé pour la famille des 'aph' ####
 aph_ARG <- Genus_matrix
@@ -43,4 +43,4 @@ for (i in 1:nrow(Genus_matrix))
 all_dist <- dist(aph_ARG, method = 'binary')
 
 clust <- hclust(all_dist, "complete")
-plot(clust, labels = FALSE)
+plot(clust)
