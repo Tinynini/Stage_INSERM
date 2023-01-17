ef2998dc5bce35bd31a7e3a453c713624fbb1179
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-01-17 17:23:03 +0100

    Update main.sh

--- Script/New_Bash_Script/main.sh
+++ Script/New_Bash_Script/main.sh
@@ -3,15 +3,18 @@
 dir1="/home/ninon.robin/gff/"
 dir2="/home/ninon.robin/usefull/"
 dir3="/home/ninon.robin/listes_id/"
-dir4="/home/ninon.robin/listes_gene/"
+dir4="/home/ninon.robin/listes_ARG/"
+dir5="/home/ninon.robin/listes_gene/"
+dir6="/home/ninon.robin/listes_seq/"
 
 end1='_usefull.txt'
 end2='_liste_id.txt'
 end3='_uniq_liste_id.txt'
-end4='_uniq_liste_gene.txt'
+end4='_liste_ARG.txt'
+end5='_uniq_liste_gene.txt'
 
 # 1 Extraction des sseqid depuis diamond avec sseqid_extractor.py --> sseqid.txt --> uniq_sseqid.txt
-
+<< 'comment'
 ./sseqid_extractor.py
 
 cat sseqid.txt | sort | uniq > uniq_sseqid.txt
@@ -33,18 +36,27 @@ for file in `ls $dir3`
 do
     fichier=$dir3$file
     filename=`echo "$file" | cut -f 1 -d '.' | cut -f 1-2 -d '_'`
+    cat uniq_sseqid.txt | grep $filename > $dir4$filename$end4
     cat $fichier | sort | uniq > $dir3$filename$end3
     rm $fichier
 done
 
-for file in `ls $dir4` 
+for file in `ls $dir5` 
 do
-    fichier=$dir4$file
+    fichier=$dir5$file
     filename=`echo "$file" | cut -f 1 -d '.' | cut -f 1-2 -d '_'`
-    cat $fichier | uniq > $dir4$filename$end4
+    cat $fichier | uniq > $dir5$filename$end5
     rm $fichier
 done
-
+comment
 # 4 Extraction des séquences associées aux gènes de la liste --> liste_seq.ffn
 
 ./seq_extractor.py 
+
+all_seq='all_seq.ffn'
+
+for file in `ls $dir6` 
+do
+    fichier=$dir6$file
+    cat $fichier 
+done > $all_seq
