caaca371508a75579e5f18d0506bbff9d15fcc3b
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-01-17 17:21:35 +0100

    Update seq_extractor.py

--- Script/New_Bash_Script/seq_extractor.py
+++ Script/New_Bash_Script/seq_extractor.py
@@ -5,10 +5,13 @@ import os
 dir1 = "/home/ninon.robin/listes_id/"
 dir2 = "/home/ninon.robin/listes_gene/"
 dir3 = "/home/ninon.robin/ffn/"
-dir4 = "/home/ninon.robin/listes_seq/"
+dir4 = "/home/ninon.robin/listes_ARG/"
+dir5 = "/home/ninon.robin/listes_seq/"
+
 end1 = '_uniq_liste_gene.txt'
 end2 = '.ffn'
-end3 = '_liste_seq.ffn'
+end3 = '_liste_ARG.txt'
+end4 = '_liste_seq.ffn'
 
 file_list = os.listdir(dir1)
 
@@ -19,10 +22,13 @@ for file in file_list :
 
     gene = dir2 + filename + end1
     fasta = dir3 + filename + end2
-    output = dir4 + filename + end3
+    args = dir4 + filename + end3
+    output = dir5 + filename + end4
 
     liste_gene = open(gene, "r")
     ffn = open(fasta, "r")
+    ARG = open(args, "r")
+    
     liste_seq = open(output, "w")
 
     with open(fichier, "r") as liste_id :
@@ -30,21 +36,23 @@ for file in file_list :
         line = liste_gene.readlines()
         ligne = liste_id.readlines()
         elements = ffn.readlines()
+        arg = ARG.readlines()
         
         element = ' '.join(elements)
         seq = element.split('>')
-        
+
         for i in range(len(line)) :
 
             for j in range(len(ligne)) :
 
-                if line[i] == ligne[j] :
+                if line[i] == ligne[j] and ligne [j] not in arg :
 
                     liste_seq.write('>') 
-                    liste_seq.write(seq[i + 1])   
+                    liste_seq.write(seq[i + 1]) 
+                    liste_seq.write('\n')  
 
+    liste_gene.close()
+    ffn.close()
+    ARG.close()
+    liste_seq.close()
     liste_id.close()
-
-liste_gene.close()
-ffn.close()
-liste_seq.close()
