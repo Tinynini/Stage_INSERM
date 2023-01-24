cec874ce0904077884276830e0c4db274037e943
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-01-24 13:52:45 +0100

    Add files via upload

---
+++ Script/New_Bash_Script/seq_reductor.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import os
+
+all_seq = open('/home/ninon.robin/all_seq.ffn', "r")
+clean_all_seq = open('/home/ninon.robin/clean_all_seq.ffn', "w")
+
+elements = all_seq.readlines()
+
+element = ''.join(elements)
+seq = element.split('>')
+
+for i in range(len(seq)) :
+   
+    sequence = ''.join(seq[i]) 
+    line = sequence.split('\n')
+
+    for j in range(len(line)) :
+        
+        if 'GCF_' not in line[j] :
+
+            clean_all_seq.write(line[j])
+            clean_all_seq.write('\n') 
+        else :
+            clean_all_seq.write('>')
+    
+all_seq.close()
+clean_all_seq.close()
+
+clean_all_seq = open('/home/ninon.robin/clean_all_seq.ffn', "r")
+uniq_all_seq = open('/home/ninon.robin/uniq_all_seq.ffn', "w")
+
+elements = clean_all_seq.readlines()
+element = ''.join(elements) 
+seq = element.split('>')
+seq[:] = [x for i, x in enumerate(seq) if i == seq.index(x)]
+
+for i in range(len(seq)) :
+
+    uniq_all_seq.write(seq[i]) 
+    uniq_all_seq.write('> seq\n')
+    
+clean_all_seq.close()
+uniq_all_seq.close()
