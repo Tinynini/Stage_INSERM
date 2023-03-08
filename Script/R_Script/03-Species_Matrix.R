3117eb7687f1d587313dd4253aefee7acd9c3a93
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-03-08 16:29:57 +0100

    Update 03-Species_Matrix.R

--- Script/R_Script/03-Species_Matrix.R
+++ Script/R_Script/03-Species_Matrix.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 
-#### Ouverture de all_species_clust.tsv (ou de Sliced_all_species_clust.tsv) & récupération des données dans une dataframe ####
+#### Ouverture de all_species_clust.tsv (ou de Sliced_all_species_clust.tsv) & recuperation des donnees dans une dataframe ####
 all_species <- read_tsv('W:/ninon-species/output/all_species_clust.tsv') %>% 
 #all_species <- read_tsv('W:/ninon-species/output/sliced_all_species_clust.tsv') %>% 
   as.data.frame()
 
-#### Prétraitement des données en vue de la création d'une matrice d'absence/présence ARGxEspèce ####
+#### Pretraitement des donnees en vue de la creation d une matrice d absence/presence ARGxEspece ####
 uni_ARG <- sort(unique(all_species$qseqid))
 n_ARG <- length(uni_ARG)
 
@@ -16,7 +16,7 @@ all_species %>%
   select(qseqid, species) %>% 
   identity() -> arg_species
 
-#### Créaction d'une matrice binaire (0/1) d'absence/présence des gènes de résistances au niveau des espèces ####
+#### Creaction d une matrice binaire (0/1) d absence/presence des genes de resistances au niveau des especes ####
 all_matrix <- matrix(data = 0, nrow = n_ARG, ncol = n_species)
 rownames(all_matrix) <- uni_ARG
 colnames(all_matrix) <- uni_species
@@ -29,6 +29,6 @@ for (i in 1:n_ARG)
   all_matrix[i, to_set] <- 1
 }
 
-#### Enregistrement de la matrice complète dans le fichier Matrix.tsv (ou de celle réduite dans le fichier New_Matrix.tsv) ####
+#### Enregistrement de la matrice complete dans le fichier Matrix.tsv (ou de celle reduite dans le fichier New_Matrix.tsv) ####
 write.table(all_matrix, "W:/ninon-species/output/Matrix.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
 #write.table(all_matrix, "W:/ninon-species/output/New_Matrix.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
