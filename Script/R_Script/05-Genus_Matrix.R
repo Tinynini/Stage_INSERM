8de3462595cea68dd8d43f6265571cd14b547c3d
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-03-08 16:37:57 +0100

    Update 05-Genus_Matrix.R

--- Script/R_Script/05-Genus_Matrix.R
+++ Script/R_Script/05-Genus_Matrix.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-#### Ouverture de all_species_clust.tsv et de Matrix.tsv (ou de Sliced_all_species_clust.tsv et de New_Matrix.tsv) & récupération des données ####
+#### Ouverture de all_species_clust.tsv et de Matrix.tsv (ou de Sliced_all_species_clust.tsv et de New_Matrix.tsv) & recuperation des donnees ####
 matrix <- read_tsv('W:/ninon-species/output/Matrix.tsv') 
 #matrix <- read_tsv('W:/ninon-species/output/New_Matrix.tsv') 
 
@@ -12,7 +12,7 @@ uni_ARG <- sort(unique(all_species$qseqid))
 rownames(matrix) <- uni_ARG
 matrix <- as.matrix(matrix)
 
-#### Prétraitement des données en vue de la création d'une matrice d'absence/présence ARGxGenus ####
+#### Pretraitement des donnees en vue de la creation d une matrice d absence/presence ARGxGenus ####
 genus <- str_replace(colnames(matrix), pattern = '(.*)_(.*)', replacement = "\\1")
 gene <- str_replace(rownames(matrix), pattern = '(.*)_(.*)_(.*)', replacement = "\\1")
 genu <- unique(genus)
@@ -20,7 +20,7 @@ genu <- unique(genus)
 colnames(matrix) <- genus
 rownames(matrix) <- gene
 
-#### Créaction d'une matrice pseudo-binaire (0/1~n) d'absence/présence des gènes de résistances au niveau des génus ####
+#### Creaction d'une matrice pseudo-binaire (0/1~n) d absence/presence des genes de resistances au niveau des genus ####
 Genus_ARG <- matrix(data = 0, nrow = length(uni_ARG), ncol = length(genu))
 
 colnames(Genus_ARG) <- genu
@@ -45,6 +45,6 @@ for (i in 1:l_1)
   }
 }
 
-#### Enregistrement de la matrice complète dans le fichier M_Genus_ARG.tsv (ou de celle réduite dans le fichier New_M_Genus_ARG.tsv) ####
+#### Enregistrement de la matrice complete dans le fichier M_Genus_ARG.tsv (ou de celle reduite dans le fichier New_M_Genus_ARG.tsv) ####
 write.table(Genus_ARG, "W:/ninon-species/output/M_Genus_ARG.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
 #write.table(Genus_ARG, "W:/ninon-species/output/New_M_Genus_ARG.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
