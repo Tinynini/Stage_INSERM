c49369f59d51c657279e1adead6b4d74f3c05460
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-03-10 12:32:16 +0100

    Update 19-Order_Tree_Parser.R

--- Script/R_Script/19-Order_Tree_Parser.R
+++ Script/R_Script/19-Order_Tree_Parser.R
@@ -3,7 +3,7 @@ library(tidytree)
 library(ape)
 library(ggtree) 
 
-#### Ouverture de Order.tree et de Taxo_result.tsv (ou de New_Taxo_result.tsv) & récupération des données ####
+#### Ouverture de Order.tree et de Taxo_result.tsv (ou de New_Taxo_result.tsv) & recuperation des donnees ####
 tree <- read.tree('W:/ninon-species/output/Order_tree.tree')
 tibble_tree <- as_tibble(tree)
 
@@ -11,7 +11,7 @@ all_species <- read_tsv('W:/ninon-species/output/Taxo_result.tsv') %>%
 #all_species <- read_tsv('W:/ninon-species/output/New_Taxo_result.tsv') %>% 
   as.data.frame()
 
-#### Prétraitement des données en vue de la création d'une matrice d'absence/présence CentroidexOrdre ####
+#### Pretraitement des donnees en vue de la creation d une matrice d absence/presence CentroidexOrdre ####
 uni_centro <- sort(unique(all_species$Centroid))
 n_centro <- length(uni_centro)
 
@@ -19,7 +19,7 @@ uni_order <- as.data.frame(sort(unique(all_species$Order)))
 colnames(uni_order) <- 'Order'
 n_order <- nrow(uni_order)
 
-#### Créaction d'une matrice binaire (0/1) d'absence/présence des centroides au niveau des ordres ####
+#### Creaction d une matrice binaire (0/1) d absence/presence des centroides au niveau des ordres ####
 centro_matrix <- matrix(data = 0, nrow = n_centro, ncol = n_order)
 rownames(centro_matrix) <- uni_centro
 
@@ -35,18 +35,18 @@ centro_matrix <- t(centro_matrix)
 centro_matrix <- as.data.frame(centro_matrix)
 centro_matrix <- cbind(uni_order, centro_matrix)
 
-#### Join de l'arbre et de la matrice & préparation de nouvelles listes ####
+#### Join de l arbre et de la matrice & preparation de nouvelles listes ####
 tibble_tree <- left_join(tibble_tree, centro_matrix, by = c('label' = 'Order'))
 
 n_ARG <- ncol(tibble_tree)
-tree_list <- vector(mode = 'list', length = n_centro - 12)# Future liste des sous-arbres par centroides (12 = n_arbres_null, obtenu restrospectivement)
+tree_list <- vector(mode = 'list', length = n_centro - 12)# Future liste des sous-arbres par centroides (12 = n_arbres_null obtenu restrospectivement)
 
 length <- as.data.frame(matrix(data = 0, nrow = n_centro, ncol = 1))
 uni_centro <- cbind(uni_centro, length) # Future dataframe des longueurs totales des sous-arbres par centroides
 colnames(uni_centro) <- c('centroid', 'length')
 j <- 1
 
-#### Création des listes des sous-arbres et de leurs longueurs par centroides ####
+#### Creation des listes des sous-arbres et de leurs longueurs par centroides ####
 for (i in 5:n_ARG)  
 {
   wanted_ARG <- colnames(tibble_tree[, i])
@@ -69,15 +69,15 @@ uni_centro <- uni_centro[-c(err),]
 names(tree_list) <- uni_centro[, 'centroid']
 n_centro <- nrow(uni_centro)
 
-#### Plot de l'histogramme des distances ####
+#### Plot de l histogramme des distances ####
 order_length <- uni_centro['length']
 oplot <- ggplot(order_length, aes(length)) + geom_histogram(bins = n_centro)
 oplot + ggtitle("Nombres d'occurrences des valeurs de distances inter-ordres") + xlab("valeurs des distances") + ylab("Nombres d'occurrences")
 
-#### Exemple de plot d'un sous_arbre avec "mef(B)_1_FJ196385" ####
+#### Exemple de plot d un sous_arbre avec "mef(B)_1_FJ196385" ####
 plot.phylo(tree_list[[1251]], show.node.label = TRUE, main = uni_centro[1251, 1], sub = uni_centro[1251, 2])
 
-#### Plot des sous-arbres des ordres par centroides sur l'arbre complet ####
+#### Plot des sous-arbres des ordres par centroides sur l arbre complet ####
 liste <- vector(mode = 'list', length = n_centro)
 
 for (i in 1:n_centro)
