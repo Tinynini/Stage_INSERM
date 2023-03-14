c22c1357f14c3a8e826f334e8829c32ae920938b
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-03-14 18:26:56 +0100

    Update 07-Taxonomy_Parser.R

--- Script/R_Script/07-Taxonomy_Parser.R
+++ Script/R_Script/07-Taxonomy_Parser.R
@@ -1,88 +1,66 @@
 library(tidyverse)
 
-#### Ouverture de bac120_taxonomy_r95.tsv et de bac120_taxonomy_r95_new.tsv & recuperation des donnees ####
-taxonomy_V1 <- read.csv('W:/ninon-species/data/bac120/bac120_taxonomy_r95.tsv', sep = ';', header = FALSE)
-taxonomy_V2 <- read.csv('W:/ninon-species/data/bac120/bac120_taxonomy_r95_new.tsv', sep = ';', header = FALSE) 
-colnames(taxonomy_V1) <- c('Occurrence', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species')
-colnames(taxonomy_V2) <- c('sseqid', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species')
+#### Ouverture de bac120_taxonomy_r95.tsv (ou de bac120_taxonomy_r95_new.tsv pour la 2nd version) & recuperation des donnees ####
+taxonomy <- read.csv('W:/ninon-species/data/bac120/bac120_taxonomy_r95.tsv', sep = ';', header = FALSE)
+#taxonomy <- read.csv('W:/ninon-species/data/bac120/bac120_taxonomy_r95_new.tsv', sep = ';', header = FALSE) 
+colnames(taxonomy) <- c('Occurrence', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species')
+#colnames(taxonomy) <- c('sseqid', 'Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species')
 
-#### Modification de la nomenclature du join avec nos propre donnees ####
-nomenclature_cleaner <- function(taxonomy)
+#### Modification de la nomenclature de la table de taxonomie en vue de son join avec nos propre donnees ####
+for (i in 2:ncol(taxonomy)) # Suppression des prefix indiquant le niveau taxonomique (superflus car deja indique par les nom de colonnes)
 {
-  for (i in 2:ncol(taxonomy)) # Suppression des prefix indiquant le niveau taxonomique (superflus car deja indique par les noms de colonnes)
-  {
-    taxonomy[,i] <- str_replace(taxonomy[,i], pattern = '(...)(.*)', replacement = "\\2")
-  }
-  
-  for (j in 1:nrow(taxonomy)) # Suppression des supplements de matricule en suffix chez les especes 'sp' (egalement superflus)
-  {
-    if (str_replace(taxonomy[j,'Species'], pattern = "(.*) (..)(.*)", replacement = "\\2") == 'sp')
-    {
-      taxonomy[j,'Species'] <- str_replace(taxonomy[j,'Species'], pattern = "(.*) (..)(.*)", replacement = "\\1\\ \\2")
-    }
-  }
-  
-  taxonomy %>% 
-    arrange(Species) %>% 
-    identity() -> taxonomy
+  taxonomy[,i] <- str_replace(taxonomy[,i], pattern = '(...)(.*)', replacement = "\\2")
 }
 
-#### Elimination de certaines sous-classifications propres a l'etude dont est tiree cette table (et parasites pour la notre !) ####
-sub_class_cleaner <- function(taxonomy)
+for (j in 1:nrow(taxonomy)) # Suppression des supplements de matricule en suffix chez les especes 'sp' (egalement superflus)
 {
-  taxonomy <- rev(unique(taxonomy))
-  
-  for (k in 2:(ncol(taxonomy) - 1)) # On commence a 2 car la colonne des especes n a pas besoin d etre traitee 
+  if (str_replace(taxonomy[j,'Species'], pattern = "(.*) (..)(.*)", replacement = "\\2") == 'sp')
   {
-    sub <- grep('(.*)_(.*)', taxonomy[, k])
-    taxonomy[sub, k] <- str_replace(taxonomy[sub, k], '(.*)_(.*)', '\\1')
+    taxonomy[j,'Species'] <- str_replace(taxonomy[j,'Species'], pattern = "(.*) (..)(.*)", replacement = "\\1\\ \\2")
   }
-  
-  sub_espece1 <- grep('(.*)_(.*) (.*)', taxonomy[, 1])
-  taxonomy[sub_espece1, 1] <- str_replace(taxonomy[sub_espece1, 1], '(.*)_(.*) (.*)', '\\1\\ \\3')
-  
-  sub_espece2 <- grep('(.*) (.*)_(.*)', taxonomy[, 1])
-  taxonomy[sub_espece2, 1] <- str_replace(taxonomy[sub_espece2, 1], '(.*) (.*)_(.*)', '\\1\\ \\2')
-
-  taxonomy <- unique(taxonomy)
 }
 
-#### Traitement preventif pour eviter la creation de certains doublons lors du join ####
-prev_doublon_cleaner <- function(taxonomy)
+taxonomy %>% 
+  arrange(Species) %>% 
+  identity() -> taxonomy
+
+taxonomy <- taxonomy[,-c(1)] # Pour la 1ere version uniquement
+taxonomy <- rev(unique(taxonomy))
+
+#### Elimination de certaines sous-classifications propres a l etude dont est tiree cette table (et parasites pour la notre !) ####
+for (k in 2:(ncol(taxonomy) - 1)) # On commence a 2 car la colonne des especes n a pas besoin d etre traitee 
 {
-  sub2 <- grep('Bacillus', taxonomy[, 'Genus'])
-  taxonomy[sub2,'Family'] <- 'Bacillaceae'
-  taxonomy[sub2, 'Order'] <- 'Bacillales'
-  
-  sub3 <- grep('Ruminococcus sp', taxonomy[, 'Species'])
-  taxonomy[sub3, 'Family'] <- 'Ruminococcaceae'
-  taxonomy[sub3, 'Order'] <- 'Oscillospirales'
-  
-  sub4 <- grep('Clostridium sp', taxonomy[, 'Species'])
-  taxonomy[sub4, 'Family'] <- 'Clostridiaceae'
-  taxonomy[sub4, 'Order'] <- 'Clostridiales'
-  
-  sub5 <- grep('Eubacterium sp', taxonomy[, 'Species'])
-  taxonomy[sub5, 'Family'] <- 'Lachnospiraceae'
-  taxonomy[sub5, 'Order'] <- 'Lachnospirales'
-  
-  taxonomy <- unique(taxonomy)
+  sub <- grep('(.*)_(.*)', taxonomy[, k])
+  taxonomy[sub, k] <- str_replace(taxonomy[sub, k], '(.*)_(.*)', '\\1')
 }
 
-#### Main ####
-taxonomy_V1 <- nomenclature_cleaner(taxonomy_V1)
-taxonomy_V2 <- nomenclature_cleaner(taxonomy_V2)
+sub_espece1 <- grep('(.*)_(.*) (.*)', taxonomy[, 1])
+taxonomy[sub_espece1, 1] <- str_replace(taxonomy[sub_espece1, 1], '(.*)_(.*) (.*)', '\\1\\ \\3')
 
-taxonomy_V1 <- taxonomy_V1[,-c(1)]
+sub_espece2 <- grep('(.*) (.*)_(.*)', taxonomy[, 1])
+taxonomy[sub_espece2, 1] <- str_replace(taxonomy[sub_espece2, 1], '(.*) (.*)_(.*)', '\\1\\ \\2')
 
-taxonomy_V1 <- sub_class_cleaner(taxonomy_V1)
-taxonomy_V2 <- sub_class_cleaner(taxonomy_V2)
+#taxonomy <- rev(unique(taxonomy)) # Pour la 2nd version uniquement
 
-taxonomy_V2 <- rev(unique(taxonomy_V2)) 
+#### Traitement préventif des donnees pour eviter la creation de certains doublons lors du join ####
+sub2 <- grep('Bacillus', taxonomy[, 'Genus'])
+taxonomy[sub2,'Family'] <- 'Bacillaceae'
+taxonomy[sub2, 'Order'] <- 'Bacillales'
 
-taxonomy_V1 <- prev_doublon_cleaner(taxonomy_V1)
-taxonomy_V2 <- prev_doublon_cleaner(taxonomy_V2)
+sub3 <- grep('Ruminococcus sp', taxonomy[, 'Species'])
+taxonomy[sub3, 'Family'] <- 'Ruminococcaceae'
+taxonomy[sub3, 'Order'] <- 'Oscillospirales'
 
-#### Enregistrement des 2 versions de la table de taxonomie dans les fichier Parsed_taxonomy.tsv et New_Parsed_taxonomy.tsv ####
-write.table(taxonomy_V1, "W:/ninon-species/output/Table_taxonomie/Parsed_taxonomy.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
-write.table(taxonomy_V2, "W:/ninon-species/output/Table_taxonomie/New_Parsed_taxonomy.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
+sub4 <- grep('Clostridium sp', taxonomy[, 'Species'])
+taxonomy[sub4, 'Family'] <- 'Clostridiaceae'
+taxonomy[sub4, 'Order'] <- 'Clostridiales'
+
+sub5 <- grep('Eubacterium sp', taxonomy[, 'Species'])
+taxonomy[sub5, 'Family'] <- 'Lachnospiraceae'
+taxonomy[sub5, 'Order'] <- 'Lachnospirales'
+
+taxonomy <- unique(taxonomy)
+
+#### Enregistrement de la table de taxonomie dans le fichier Parsed_taxonomy.tsv (ou dans le fichier New_Parsed_taxonomy.tsv pour la 2nd version) ####
+write.table(taxonomy, "W:/ninon-species/output/Table_taxonomie/Parsed_taxonomy.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
+#write.table(taxonomy, "W:/ninon-species/output/Table_taxonomie/New_Parsed_taxonomy.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
