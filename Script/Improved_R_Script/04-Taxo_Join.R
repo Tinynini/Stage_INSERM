4c84c3403dd422681152aae918297f78b49658a3
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-03-14 15:09:23 +0100

    Update 04-Taxo_Join.R

--- Script/Improved_R_Script/04-Taxo_Join.R
+++ Script/Improved_R_Script/04-Taxo_Join.R
@@ -9,7 +9,7 @@ level_name[1] <- 'species'
 suffix <- c('', '(.*) (bacterium)', '(.*)(ceae) (bacterium)', '(.*)(ales) (bacterium)')
 cond <- c('', FALSE, TRUE, TRUE)
 
-all_species <- read_tsv('W:/ninon-species/output/Output_M2/Dataframe/sliced_all_species_clust.tsv') %>%
+all_species <- read_tsv('W:/ninon-species/output/Output_M2/ARG/Dataframe/sliced_all_species_clust.tsv') %>%
   as.data.frame()
 
 all_species[, 'species'] <- str_replace(all_species[, 'species'], pattern = '(.*)(_)(.*)', replacement = "\\1\\ \\3")
@@ -119,4 +119,4 @@ all_species[ex4, 'Order'] <- 'Lachnospirales'
 
 na_species <- as.data.frame(unique(all_species[is.na(all_species[, 'Domain']), 'species']))
 
-write.table(all_species, "W:/ninon-species/output/Output_M2/Dataframe/Sliced_ARG_Species.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
+write.table(all_species, "W:/ninon-species/output/Output_M2/ARG/Dataframe/Sliced_ARG_Species.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
