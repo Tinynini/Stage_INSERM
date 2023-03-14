19b782e692d6183cf8138d78c285c9336dcc2fb8
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-03-14 13:56:00 +0100

    Update 08-Taxonomy_Espece_Parser.R

--- Script/R_Script/08-Taxonomy_Espece_Parser.R
+++ Script/R_Script/08-Taxonomy_Espece_Parser.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 
 #### Ouverture de Parsed_taxonomy.tsv et de all_species_clust.tsv (ou de Sliced_all_species_clust.tsv) & recuperation des donnees ####
-Parsed_taxonomy <- read_tsv('W:/ninon-species/output/Parsed_taxonomy.tsv') 
+Parsed_taxonomy <- read_tsv('W:/ninon-species/output/Table_taxonomie/Parsed_taxonomy.tsv') 
 
-all_species <- read_tsv('W:/ninon-species/output/all_species_clust.tsv') %>% 
-#all_species <- read_tsv('W:/ninon-species/output/sliced_all_species_clust.tsv') %>%
+all_species <- read_tsv('W:/ninon-species/output/Output_M1/Dataframe/all_species_clust.tsv') %>% 
+#all_species <- read_tsv('W:/ninon-species/output/Output_M1/Dataframe/sliced_all_species_clust.tsv') %>%
   as.data.frame()
 
 #### Creation d une nouvelle dataframe a partir des colonnes de notre dataframe actuelle selectionnees et reordonnees selon nos besoins ####
@@ -36,5 +36,5 @@ ARG_Species <- left_join(ARG_Species, Parsed_taxonomy, by = c('species' = 'Speci
 na_species <- as.data.frame(unique(ARG_Species[is.na(ARG_Species[, 'Genus']), 'species'])) # Especes n ayant pas pu etre matchees avec celle de la table de taxonomie
 
 #### Enregistrement de la nouvelle dataframe complete dans le fichier ARG_Species.tsv (ou de celle slicee dans le fichier New_ARG_Species.tsv) ####
-write.table(ARG_Species, "W:/ninon-species/output/ARG_Species.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
-#write.table(ARG_Species, "W:/ninon-species/output/New_ARG_Species.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
+write.table(ARG_Species, "W:/ninon-species/output/Output_M1/Dataframe/ARG_Species.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
+#write.table(ARG_Species, "W:/ninon-species/output/Output_M1/Dataframe/New_ARG_Species.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
