07fe5c8c810b31215723c1b3198e7b5517163f12
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-03-14 13:18:31 +0100

    Update 05-Matrix_Binaire.R

--- Script/Improved_R_Script/05-Matrix_Binaire.R
+++ Script/Improved_R_Script/05-Matrix_Binaire.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-all_species <- read_tsv('W:/ninon-species/output/Sliced_ARG_Species.tsv') %>% 
+all_species <- read_tsv('W:/ninon-species/output/Dataframe/Dataframe_M2/Sliced_ARG_Species.tsv') %>% 
   as.data.frame()
 
 level <- as.data.frame(all_species[, c(7:12)])
@@ -32,7 +32,7 @@ for (i in 1:6)
     centro_matrix[j, to_set] <- 1                       
   }
 
-  path_start = "W:/ninon-species/output/Sliced_Matrix_"
+  path_start = "W:/ninon-species/output/Matrice/Matrice_M2/Sliced_Matrix_"
   path_end = ".tsv"
   file_name = str_glue("{path_start}{level_name[i]}{path_end}")
 
