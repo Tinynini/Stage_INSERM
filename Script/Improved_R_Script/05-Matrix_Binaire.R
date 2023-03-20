9a84c2ef522dfc89049ec39824cbc8cc8edda731
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-03-20 13:02:07 +0100

    Update 05-Matrix_Binaire.R

--- Script/Improved_R_Script/05-Matrix_Binaire.R
+++ Script/Improved_R_Script/05-Matrix_Binaire.R
@@ -1,27 +1,27 @@
 library(tidyverse)
 
-#### Ouverture de Sliced_all_species_clust.tsv & recuperation des donnees dans une dataframe ####
+#### Ouverture de Sliced_ARG_Species.tsv & recuperation des donnees dans une dataframe ####
 all_species <- read_tsv('W:/ninon-species/output/Output_M2/ARG/Dataframe/Sliced_ARG_Species.tsv') %>% 
-  as.data.frame() # On ouvre le fichier sous la forme d une dataframe
+  as.data.frame() 
 
 #### Pretraitement des donnees en vue de la creation de matrices d absence/presence ARGxLevel ####
 level <- as.data.frame(all_species[, c(7:12)]) # On extrait le contenu des colonnes associees aux 6 niveaux taxonomiques etudies
 level_name <- unlist(colnames(all_species[, c(7:12)])) # On extrait aussi leurs labels pour pouvoir travailler a un niveau donne plus facilement
 
-uni_centro <- sort(unique(all_species$Centroid)) # On extrait la colonne des centroid en appliquant sort(unique()) dessus pour les trier en les dedoublonnant 
-n_centro <- length(uni_centro) # On recupere le nombre de centroids distincts
+uni_centro <- sort(unique(all_species$Centroid)) # On extrait la colonne des centroids
+n_centro <- length(uni_centro) 
 
 for (i in 1:6) # Permet de parcourir les 6 niveaux taxonomiques etudies (d espece a phylum)
 {
-  uni_level <- as.data.frame(sort(unique(level[, i]))) # On extrait la colonne du niveau i en appliquant sort(unique()) dessus pour le trier en le dedoublonnant 
-  colnames(uni_level) <- level_name[i] # On renomme la colonne extraite pour faciliter son utilisation a venir
-  n_level <- nrow(uni_level) # On recupere le nombre de representants distints du niveau i
+  uni_level <- as.data.frame(sort(unique(level[, i]))) # On extrait la colonne du niveau i 
+  colnames(uni_level) <- level_name[i] 
+  n_level <- nrow(uni_level) 
   
   #### Creaction d une matrice binaire (0/1) d absence/presence des genes de resistances au niveau traite ####
-  centro_matrix <- matrix(data = 0, nrow = n_centro, ncol = n_level) # Dimensionnee selon les 2 valeurs recuperee precedemment & remplie de 0 pour l instant
-  rownames(centro_matrix) <- uni_centro # On associe les noms de centroids aux lignes
-  colnames(centro_matrix) <- uni_level[, 1] # Et ceux des representant du niveau i aux colonnes 
-  # N.B. : On aurait tres bien pu faire le contraire c est kif-kif
+  centro_matrix <- matrix(data = 0, nrow = n_centro, ncol = n_level) 
+  rownames(centro_matrix) <- uni_centro 
+  colnames(centro_matrix) <- uni_level[, 1]  
+  
   all_species %>% 
     select(Centroid, level_name[i]) %>% # On selectionne les colonnes du niveau i et des centroids
     identity() -> arg_level
@@ -36,9 +36,9 @@ for (i in 1:6) # Permet de parcourir les 6 niveaux taxonomiques etudies (d espec
   }
   
   #### Enregistrement de la matrice binaire ainsi obtenue dans un fichier nominatif ####
-  path_start = "W:/ninon-species/output/Output_M2/ARG/Matrice/Sliced_Matrix_" # Chemin d acces + debut de nom de fichier
-  path_end = ".tsv" # Fin de nom de fichier (== extension du fichier)
-  file_name = str_glue("{path_start}{level_name[i]}{path_end}") # Assemblage des 2 autour du label du niveau i pour obtenir le nom de fichier complet
+  path_start = "W:/ninon-species/output/Output_M2/ARG/Matrice/Sliced_Matrix_" 
+  path_end = ".tsv" 
+  file_name = str_glue("{path_start}{level_name[i]}{path_end}") # Le nom de fichier est definit comme une variable
   
-  write.table(centro_matrix, file_name, sep = '\t', row.names = FALSE, col.names = TRUE) # Methode habituelle d enregistrement d une structure de type table dans un fichier
+  write.table(centro_matrix, file_name, sep = '\t', row.names = FALSE, col.names = TRUE) 
 }
