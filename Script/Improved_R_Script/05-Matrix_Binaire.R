42abaea6bd51a6061c7607d10a6e768731da3e2d
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-04-12 11:01:31 +0200

    Update 05-Matrix_Binaire.R

--- Script/Improved_R_Script/05-Matrix_Binaire.R
+++ Script/Improved_R_Script/05-Matrix_Binaire.R
@@ -4,12 +4,12 @@ library(tidyverse)
 all_species <- read_tsv('W:/ninon-species/output/Output_M2/ARG/Dataframe/Sliced_ARG_Species.tsv') %>% 
   as.data.frame() 
 
-#### Pretraitement des donnees en vue de la creation de matrices d absence/presence ARGxLevel ####
+#### Pretraitement des donnees en vue de la creation de matrices d absence/presence GenexLevel ####
 level <- as.data.frame(all_species[, c(7:12)]) # On extrait le contenu des colonnes associees aux 6 niveaux taxonomiques etudies
 level_name <- unlist(colnames(all_species[, c(7:12)])) # On extrait aussi leurs labels pour pouvoir travailler a un niveau donne plus facilement
 
-uni_ARG <- sort(unique(all_species$qseqid)) # On extrait la colonne des ARGs
-n_ARG <- length(uni_ARG) 
+uni_gene <- sort(unique(all_species$qseqid)) # On extrait la colonne des genes
+n_gene <- length(uni_gene) 
 
 for (i in 1:6) # Permet de parcourir les 6 niveaux taxonomiques etudies (d espece a phylum)
 {
@@ -17,22 +17,22 @@ for (i in 1:6) # Permet de parcourir les 6 niveaux taxonomiques etudies (d espec
   colnames(uni_level) <- level_name[i] 
   n_level <- nrow(uni_level) 
   
-  #### Creaction d une matrice binaire (0/1) d absence/presence des genes de resistances au niveau i ####
-  ARG_matrix <- matrix(data = 0, nrow = n_ARG, ncol = n_level) 
-  rownames(ARG_matrix) <- uni_ARG 
-  colnames(ARG_matrix) <- uni_level[, 1]  
+  #### Creaction d une matrice binaire (0/1) d absence/presence des genes au niveau i ####
+  gene_matrix <- matrix(data = 0, nrow = n_gene, ncol = n_level) 
+  rownames(gene_matrix) <- uni_gene 
+  colnames(gene_matrix) <- uni_level[, 1]  
   
-  all_species %>% # On selectionne les colonnes du niveau i et des ARGs
+  all_species %>% # On selectionne les colonnes du niveau i et des genes
     select(qseqid, level_name[i]) %>% 
-    identity() -> arg_level
+    identity() -> gene_level
   
-  for (j in 1:n_ARG) # Permet de parcourir les j ARGs distincts
+  for (j in 1:n_gene) # Permet de parcourir les j genes distincts
   {
-    curr_ARG <- uni_ARG[j] # Pour l ARG j
-    curr_level <- arg_level[arg_level$qseqid == curr_ARG, level_name[i]] # Au niveau i
+    curr_gene <- uni_gene[j] # Pour le gene j
+    curr_level <- gene_level[gene_level$qseqid == curr_gene, level_name[i]] # Au niveau i
     
-    to_set <- which(uni_level[, level_name[i]] %in% curr_level) # On extrait les representants du niveau i qui matchent l ARG j
-    ARG_matrix[j, to_set] <- 1 # On attribue la valeur 1 aux cases associees a ces matchs dans la matrice                    
+    to_set <- which(uni_level[, level_name[i]] %in% curr_level) # On extrait les representants du niveau i qui matchent le gene j
+    gene_matrix[j, to_set] <- 1 # On attribue la valeur 1 aux cases associees a ces matchs dans la matrice                    
   }
   
   #### Enregistrement de la matrice binaire ainsi obtenue dans un fichier nominatif ####
@@ -40,5 +40,5 @@ for (i in 1:6) # Permet de parcourir les 6 niveaux taxonomiques etudies (d espec
   path_end <- ".tsv" 
   file_name <- str_glue("{path_start}{level_name[i]}{path_end}") # Le nom de fichier est definit par une variable
   
-  write.table(ARG_matrix, file_name, sep = '\t', row.names = FALSE, col.names = TRUE) 
+  write.table(gene_matrix, file_name, sep = '\t', row.names = FALSE, col.names = TRUE) 
 }
