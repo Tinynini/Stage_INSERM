db0a1c97e910a3baa34c182bb5df82acd4941efe
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-04-12 14:15:02 +0200

    Update 09-Tree_Pruner.R

--- Script/Improved_R_Script/09-Tree_Pruner.R
+++ Script/Improved_R_Script/09-Tree_Pruner.R
@@ -28,7 +28,6 @@ for (i in 1:6) # Permet de parcourir les 6 niveaux taxonomiques etudies (d espec
   taxo_tree <- left_join(tree_df, small_taxo, by = c('label' = 'sseqid')) # On join l arbre et les colonnes de la table de taxonomie sur les colonnes de labels de part et d autre
   taxo_tree[1:Ntip(tree), 'label'] <- taxo_tree[1:Ntip(tree), colnames(taxo[i])] # On remplace le contenu de la colonne des labels par celui de la colonne du niveau i pour les labels de tips uniquement (surtout pas ceux de nodes !!) 
   taxo_tree <- taxo_tree[, -5] # On supprime la colonne du niveau i
-  
   Label <- taxo_tree[1:Ntip(tree),] # On extrait les lignes associers aux labels de tips
 
   Label %>% # Permet d isoler les lignes qui ont le meme contenu pour la colonne 'label' (== doublons) et de n en garder qu une seule a chaque fois
@@ -42,7 +41,6 @@ for (i in 1:6) # Permet de parcourir les 6 niveaux taxonomiques etudies (d espec
 
   # On extrait de nos lignes toutes celles qui ne font pas partie du groupe qu on a isole (== les doublons)
   tips <- which((Label[, 'node'] %>% pull()) %in% (labels[, 'node'] %>% pull()) == FALSE)
-
   new_tree <- as.phylo(taxo_tree) # On passe au format phylo pour pouvoir pruner l arbre
   new_tree <- drop.tip(new_tree, tips) # On prune l arbre en supprimant les lignes associees aux doublons
   taxo_tree <- as_tibble(new_tree) # On passe au format tibble plus pratique a manipuler
@@ -50,17 +48,13 @@ for (i in 1:6) # Permet de parcourir les 6 niveaux taxonomiques etudies (d espec
   #### Join de l arbre avec les colonnes du niveau i et de son partage (== transposition de l arbre a nos propres donnees) ####
   phylo_tree <- left_join(taxo_tree, uni_level, by = c('label' = 'level')) # On join l arbre et les colonnes du niveau i et de son partage sur la colonne des labels et celle du niveau i
   n_tips <- nrow(phylo_tree) - Nnode(new_tree) # Permet de recuperer le nombre de tips malgres l apparition de nombreux doublons
-  
   na_label <- is.na(phylo_tree[1:n_tips, 'share']) # On testes les lignes associees aux labels de tips pour savoir lesquelles ont un partage non renseigne (NA)
   level <- which(na_label == FALSE) # On isole les lignes dont le partage est bien renseigne (== celles qui matchent nos propres donnees)
-  
   phylo_tree[level, 'share'] <- 1 # On unifie la valeur des partages pour ces lignes pour rendre les doublons post-join a nouveau decelables
   phylo_tree <- unique(phylo_tree) # On dedoublonne notre arbre (operation impossible sans unifier d abbord les valeurs de partage)
-
   na_label <- is.na(phylo_tree[1:Ntip(new_tree), 'share']) # On refait le meme test en remettant a jour le nombre de tips
   level <- which(na_label == TRUE) # Cette fois on isole les lignes dont le partage n est pas renseigne (== celles qui ne matchent pas nos propres donnees)
   # N.B. : La presence de la colonne des partages du niveau i etait donc bien necessaire car elle seule nous permet de savoir quels tips ont ete matches ou non lors du join
-
   next_tree <- as.phylo(phylo_tree) # On passe au format phylo pour pouvoir pruner l arbre
   next_tree <- drop.tip(next_tree, level) # On prune l arbre en supprimant les lignes associees aux doublons
   phylo_tree <- as_tibble(next_tree) # On passe au format tibble plus pratique a manipuler
