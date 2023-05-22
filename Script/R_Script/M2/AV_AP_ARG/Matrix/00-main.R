2647281710cbd9f277ac7424458215f2f5cb2c59
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-05-22 14:40:39 +0200

    Update 00-main.R

--- Script/R_Script/M2/AV_AP_ARG/Matrix/00-main.R
+++ Script/R_Script/M2/AV_AP_ARG/Matrix/00-main.R
@@ -6,30 +6,18 @@ library(ggtree)
 #### A faire : ####
 
 # 1 : Remarque Nicolas (table excel) + 3 nouveaux graphs (photo antoine)
-# 2 : Switcher en bash pour pouvoir choisir si on travaille en sliced ou total (Tous sauf 01 et 03)/en anglais ou français (07 08 et 10)/avec quels reps (07 et 10) a l avance !!
+# 2 : Switcher en bash pour pouvoir choisir si on travaille en sliced ou total (Tous sauf 01 et 03)/en anglais ou franÃ§ais (07 08 et 10)/avec quels reps (07 et 10) a l avance !!
 # 3 (Optionnel) : Checker l existance des inputs avant 05 06 et 09 (comment gerer ca pour plusieurs inputs ?) ??
 
 #### Main : ####
 
 # N.B. : Verifier les graphs sur l ensemble des especes (pas possible juste avec les vibrio)
 
-# if (file.exists('W:/ninon-species/output/Output_M2/ARG/Dataframe/all_species.tsv') == FALSE)
-# {
-#   source('W:/ninon-species/script/Script_M2/01-Species_filtering.R') # /!\ Ca prend 3/4h a 2h !!
-# } else if (file.exists('W:/ninon-species/output/Output_M2/ARG/Dataframe/sliced_all_species_clust.tsv') == FALSE)
-# {
-#   source('W:/ninon-species/script/Script_M2/02-All_species_cluster.R') 
-# } else 
-# {
-#   source('W:/ninon-species/script/Script_M2/03-Taxonomy_parser.R') 
-# }
-
 if (file.exists('W:/ninon-species/output/Output_M2/AV_AP_ARG/Dataframe/sliced_all_species_clust') == FALSE)
 {
   source('W:/ninon-species/script/Script_M2/01-02_alt.R') 
 }
 
-#if (file.exists('W:/ninon-species/output/Output_M2/ARG/Dataframe/sliced_all_species_taxo.tsv') == FALSE)
 if (file.exists('W:/ninon-species/output/Output_M2/AV_AP_ARG/Dataframe/sliced_all_species_taxo.tsv') == FALSE)
 {
   source('W:/ninon-species/script/Script_M2/04-Taxo_Join.R') 
