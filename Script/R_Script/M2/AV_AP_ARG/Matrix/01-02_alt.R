b5224807d3d5bf9bea9ba67db23143d44b9b4669
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-05-23 13:50:31 +0200

    Update 01-02_alt.R

--- Script/R_Script/M2/AV_AP_ARG/Matrix/01-02_alt.R
+++ Script/R_Script/M2/AV_AP_ARG/Matrix/01-02_alt.R
@@ -13,46 +13,34 @@ matrix <- read.csv('W:/ninon-species/data/vib_matrix.tsv', header = TRUE, sep = 
 species <- colnames(matrix)
 species <- species[-1]
 
-gene <- matrix[, 1]
-sharing <- matrix[, -1] 
-
-all_species <- as.data.frame(matrix(data="0", nrow=1, ncol=3))
-colnames(all_species) <- c('qseqid', 'shared_by', 'species')
-
-for (i in 1:length(gene))
+for (l in length(species)) # Inversion des 2 parties de nom d'espece pour les especes 'UNVERIFIED_ORG' pour avoir la bonne nomenclature
 {
-  
+  if (startsWith(species[l], 'UNV') == TRUE)
+  {
+    species[l] <- str_replace(species[l], pattern = "(.*)_(.*)_(.*)", replacement = "\\3\\_\\1\\_\\2.")
+  }
+}
+
+colnames(matrix) <- c('qseqid', species)
+
+sharing <- matrix[, -1] 
+all_species <- as.data.frame(matrix(data="0", nrow=1, ncol=(ncol(matrix) + 1)))
+colnames(all_species) <- c('qseqid', 'shared_by', species)
+
+for (i in 1:nrow(matrix))
+{
   share <- sum(sharing[i,])
   
   if (share > 0) # uniquement necessaire avec la matrice de test
   {
-    gene_species_share <- as.data.frame(matrix(data="0", nrow=share, ncol=3))
-    colnames(gene_species_share) <- c('qseqid', 'shared_by', 'species')
-    
-    k <- 0
-    
-    for (j in 1:length(species))
-    {
-      if (sharing[i, j] == 1)
-      {
-        k <- k + 1
-        gene_species_share[k, c('qseqid', 'shared_by', 'species')] <- c(gene[i], share, species[j])
-      }
-    }
-    
+    gene_species_share <- cbind(matrix[i, 1], share, sharing[i,])
+    colnames(gene_species_share) <- c('qseqid', 'shared_by', species)
+  
     all_species <- rbind(all_species, gene_species_share)
   }
 }
 
 all_species <- all_species[-1,]
-
-for (l in 1:nrow(all_species)) # Inversion des 2 parties de nom d'espece pour les especes 'UNVERIFIED_ORG' pour avoir la bonne nomenclature
-{
-  if (startsWith(all_species[j, 'species'], 'UNV') == TRUE)
-  {
-    all_species[j,'species'] <- str_replace(all_species[j,'species'], pattern = "(.*)_(.*)_(.*)", replacement = "\\3\\_\\1\\_\\2.")
-  }
-}
 
 #### Enregistrement de la dataframe dans le fichier sliced_all_species_clust.tsv ####
 write.table(all_species, "W:/ninon-species/output/Output_M2/AV_AP_ARG/Matrix/Dataframe/sliced_all_species_clust.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
