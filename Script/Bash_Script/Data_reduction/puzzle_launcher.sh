238c1bfaa5913d69ff8818b310737caec51e2871
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-06-01 12:32:11 +0200

    Update puzzle_launcher.sh

--- Script/Bash_Script/Data_reduction/puzzle_launcher.sh
+++ Script/Bash_Script/Data_reduction/puzzle_launcher.sh
@@ -2,6 +2,10 @@
 #wanted="Klebs.pneumo"
 #wanted="1G"
 #wanted="100M-1K"
+#wanted="100M-100K"
+#wanted="100M-10M"
+#wanted="1M-100K"
+#wanted="10K-1K"
 
 ./puzzle.sh "../data/Parsed_species/${wanted}.txt" \
             "../output/Puzzle_matrix/${wanted}_species.tsv" \
