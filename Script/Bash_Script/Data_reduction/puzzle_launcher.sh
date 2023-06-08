9de593d7fa921f82a472570bbe2c93f23c506b1f
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-06-08 11:58:07 +0200

    Update puzzle_launcher.sh

--- Script/Bash_Script/Data_reduction/puzzle_launcher.sh
+++ Script/Bash_Script/Data_reduction/puzzle_launcher.sh
@@ -1,12 +1,12 @@
 #wanted="Salm.enter"
 #wanted="Klebs.pneumo"
-
 #wanted="1G"
 #wanted="100M-1"
 #wanted="100M-2"
 #wanted="100M"
 #wanted="10M"
-#wanted="1M-100K"
+#wanted="1M"
+#wanted="100K"
 #wanted="10K-1K"
 
 ./puzzle.sh "../data/Parsed_species/${wanted}.txt" \
