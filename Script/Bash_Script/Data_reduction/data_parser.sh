cbba5de3a539204d3e762640a7b645e35c77184b
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-06-08 11:57:43 +0200

    Update data_parser.sh

--- Script/Bash_Script/Data_reduction/data_parser.sh
+++ Script/Bash_Script/Data_reduction/data_parser.sh
@@ -42,11 +42,11 @@ du -a $(cat /home/ninon.robin/alt_bash_script/data/full_species_ninon.txt) | sor
 cat alt_bash_script/data/sort_DU.txt | sed -n '1p' | cut -f 2 > alt_bash_script/data/Parsed_species/E.coli.txt
 cat alt_bash_script/data/sort_DU.txt | sed -n '2p' | cut -f 2 > alt_bash_script/data/Parsed_species/Salm.enter.txt
 cat alt_bash_script/data/sort_DU.txt | sed -n '3p' | cut -f 2 > alt_bash_script/data/Parsed_species/Klebs.pneumo.txt
-
 cat alt_bash_script/data/sort_DU.txt | sed -n '4,11p' | cut -f 2 > alt_bash_script/data/Parsed_species/1G.txt
-cat alt_bash_script/data/sort_DU.txt | sed -n '12,51p' | cut -f 2 > alt_bash_script/data/Parsed_species/100M.txt
+#cat alt_bash_script/data/sort_DU.txt | sed -n '12,51p' | cut -f 2 > alt_bash_script/data/Parsed_species/100M.txt
 cat alt_bash_script/data/sort_DU.txt | sed -n '12,21p' | cut -f 2 > alt_bash_script/data/Parsed_species/100M-1.txt
 cat alt_bash_script/data/sort_DU.txt | sed -n '22,51p' | cut -f 2 > alt_bash_script/data/Parsed_species/100M-2.txt
 cat alt_bash_script/data/sort_DU.txt | sed -n '52,221p' | cut -f 2 > alt_bash_script/data/Parsed_species/10M.txt
-cat alt_bash_script/data/sort_DU.txt | sed -n '221,3133p' | cut -f 2 > alt_bash_script/data/Parsed_species/1M-100K.txt
+cat alt_bash_script/data/sort_DU.txt | sed -n '221,780p' | cut -f 2 > alt_bash_script/data/Parsed_species/1M.txt
+cat alt_bash_script/data/sort_DU.txt | sed -n '781,3133p' | cut -f 2 > alt_bash_script/data/Parsed_species/100K.txt
 cat alt_bash_script/data/sort_DU.txt | sed -n '3134,11822p' | cut -f 2 > alt_bash_script/data/Parsed_species/10K-1K.txt
