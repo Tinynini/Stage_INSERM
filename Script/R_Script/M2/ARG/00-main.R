1d2cbb2d71e8aca58d8d94bddaf8f0d40694ac4f
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-06-09 15:56:34 +0200

    Update 00-main.R

--- Script/R_Script/M2/ARG/00-main.R
+++ Script/R_Script/M2/ARG/00-main.R
@@ -5,7 +5,7 @@ library(ggtree)
 
 #### A faire : ####
 
-# 1 :  Remarques Nicolas (table excel) + 3 nouveaux graphs (photo antoine) + Commentaires script 11
+# 1 : Remarques Nicolas (table excel) + Commentaires script 11
 # 2 : Switcher en bash pour pouvoir choisir si on travaille en sliced ou total (Tous sauf 01 et 03)/en anglais ou français (07 08 et 10)/avec quels reps (07 et 10) a l avance !!
 # 3 (Optionnel) : Checker l existance des inputs avant 05 06 et 09 (comment gerer ca pour plusieurs inputs ?) ??
 
@@ -27,10 +27,12 @@ if (file.exists('W:/ninon-species/output/Output_M2/ARG/Dataframe/sliced_all_spec
   source('W:/ninon-species/script/Script_M2/ARG/04-Taxo_Join.R') 
 } else 
 {
-  source('W:/ninon-species/script/Script_M2/ARG/05-Matrix_Binaire.R') 
-  source('W:/ninon-species/script/Script_M2/ARG/06-Matrix_Pseudo_Binaire.R') 
-  source('W:/ninon-species/script/Script_M2/ARG/07-Matrix_Plot.R') 
-  source('W:/ninon-species/script/Script_M2/ARG/08-Taxo_Filter_Plot.R') 
-  source('W:/ninon-species/script/Script_M2/ARG/09-Tree_Pruner.R') 
-  source('W:/ninon-species/script/Script_M2/ARG/10-Tree_Plot.R') 
+  source('W:/ninon-species/script/Script_M2/ARG/05-Matrix_Binaire.R')
+  source('W:/ninon-species/script/Script_M2/ARG/06-Matrix_Pseudo_Binaire.R')
+  source('W:/ninon-species/script/Script_M2/ARG/07-Matrix_Plot.R')
+  source('W:/ninon-species/script/Script_M2/ARG/08-Taxo_Filter_Plot.R')
+  source('W:/ninon-species/script/Script_M2/ARG/09-Tree_Pruner.R')
+  source('W:/ninon-species/script/Script_M2/ARG/10-Tree_Plot.R')
+  source('W:/ninon-species/script/Script_M2/ARG/11-New_graphs.R') 
+  # Max via all ou en tout cas plus simplement ??
 }
