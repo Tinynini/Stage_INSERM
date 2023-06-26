82938da1f4ecb2563f7e158ef00c1cd6e91d5e32
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-06-26 12:01:35 +0200

    Update data_parser.sh

--- Script/Bash_Script/Data_reduction/data_parser.sh
+++ Script/Bash_Script/Data_reduction/data_parser.sh
@@ -3,8 +3,8 @@
 cluster='/home/ninon.robin/Projet-Cluster/Output/cluster_fast_gene_0.95.txt'
 cat ${cluster} | grep -E -v '^C' > /home/ninon.robin/alt_bash_script/data/clust.txt
   
-IFS=$'\n' 
-
+IFS=$'\n'
+    
 for line in $(cat /home/ninon.robin/alt_bash_script/data/clust.txt)  
 do 
     if [ -n "$(echo ${line} | grep -E '^S')" ]
@@ -28,6 +28,7 @@ cat /home/ninon.robin/alt_bash_script/data/species_test_ninon.txt | sort -f -t '
 res_ninon_path="/home/ninon.robin/diamond_ninon/"
 find ${res_ninon_path} -name "*.tsv" > /home/ninon.robin/alt_bash_script/data/species_ninon.txt
 cat /home/ninon.robin/alt_bash_script/data/species_ninon.txt | sort -f -t '/' -k 4 > /home/ninon.robin/alt_bash_script/data/full_species_ninon.txt
+
 full_species='/home/ninon.robin/alt_bash_script/data/full_species_ninon.txt'
 
 while read path 
@@ -49,4 +50,5 @@ cat alt_bash_script/data/sort_DU.txt | sed -n '22,51p' | cut -f 2 > alt_bash_scr
 cat alt_bash_script/data/sort_DU.txt | sed -n '52,221p' | cut -f 2 > alt_bash_script/data/Parsed_species/10M.txt
 cat alt_bash_script/data/sort_DU.txt | sed -n '222,780p' | cut -f 2 > alt_bash_script/data/Parsed_species/1M.txt
 cat alt_bash_script/data/sort_DU.txt | sed -n '781,3133p' | cut -f 2 > alt_bash_script/data/Parsed_species/100K.txt
-cat alt_bash_script/data/sort_DU.txt | sed -n '3134,11822p' | cut -f 2 > alt_bash_script/data/Parsed_species/10K-1K.txt
+cat alt_bash_script/data/sort_DU.txt | sed -n '3134,6284p' | cut -f 2 > alt_bash_script/data/Parsed_species/10K.txt
+cat alt_bash_script/data/sort_DU.txt | sed -n '6285,11822p' | cut -f 2 > alt_bash_script/data/Parsed_species/1K.txt
