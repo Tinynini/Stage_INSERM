a3d87b91aa298b0b3cc4f2944b647c07c5d3c8db
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-06-26 12:00:39 +0200

    Update puzzle_solver.sh

--- Script/Bash_Script/Data_reduction/puzzle_solver.sh
+++ Script/Bash_Script/Data_reduction/puzzle_solver.sh
@@ -11,7 +11,8 @@ M_100_2_species='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/100M-2_s
 M_10_species='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/10M_species.tsv'
 M_1_species='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/1M_species.tsv'
 K_100_species='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/100K_species.tsv'
-K_10_species='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/10K-1K_species.tsv'
+K_10_species='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/10K_species.tsv'
+K_1_species='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/1K_species.tsv'
 
 Coli_share='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/Coli_share.tsv'
 Salm_share='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/Salm.enter_share.tsv'
@@ -22,10 +23,11 @@ M_100_2_share='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/100M-2_sha
 M_10_share='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/10M_share.tsv'
 M_1_share='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/1M_share.tsv'
 K_100_share='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/100K_share.tsv'
-K_10_share='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/10K-1K_share.tsv'
+K_10_share='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/10K_share.tsv'
+K_1_share='/home/ninon.robin/alt_bash_script/output/Puzzle_matrix/1K_share.tsv'
 
-paste ${Coli_species} ${Salm_species} ${Klebs_species} ${G_species} ${M_100_1_species} ${M_100_2_species} ${M_10_species} ${M_1_species} ${K_100_species} ${K_10_species} | sed 's/\t//g' > /home/ninon.robin/alt_bash_script/output/full_matrix.tsv
-paste ${centro} ${Coli_share} ${Salm_share} ${Klebs_share} ${G_share} ${M_100_1_share} ${M_100_2_share} ${M_10_share} ${M_1_share} ${K_100_share} ${K_10_share} | sed 's/\t//g' >> /home/ninon.robin/alt_bash_script/output/full_matrix.tsv
+paste ${Coli_species} ${Salm_species} ${Klebs_species} ${G_species} ${M_100_1_species} ${M_100_2_species} ${M_10_species} ${M_1_species} ${K_100_species}  ${K_1_species} | sed 's/\t//g' > /home/ninon.robin/alt_bash_script/output/full_matrix.tsv
+paste ${centro} ${Coli_share} ${Salm_share} ${Klebs_share} ${G_share} ${M_100_1_share} ${M_100_2_share} ${M_10_share} ${M_1_share} ${K_100_share} ${K_10_share} ${K_1_share} | sed 's/\t//g' >> /home/ninon.robin/alt_bash_script/output/full_matrix.tsv
 #paste ${species} ${Salm_species} ${Klebs_species} ${G_species} ${M_100_1_species} ${M_100_2_species} ${M_10_species} ${M_1_species} ${K_100_species} ${K_10_species} | sed 's/\t//g' > /home/ninon.robin/alt_bash_script/output/pseudo_matrix.tsv
 #paste ${centro} ${Salm_share} ${Klebs_share} ${G_share} ${M_100_1_share} ${M_100_2_share} ${M_10_share} ${M_1_share} ${K_100_share} ${K_10_share} | sed 's/\t//g' >> /home/ninon.robin/alt_bash_script/output/pseudo_matrix.tsv
 #paste ${species} ${K_100_species} | sed 's/\t//g' > /home/ninon.robin/alt_bash_script/output/100K_matrix.tsv
