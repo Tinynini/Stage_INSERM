c34e2abe07538eb3926bfd0112ad17075c51a51b
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-07-07 13:37:25 +0200

    Update 08-Taxo_Filter_Plot.R

--- Script/R_Script/M2/AV_AP_ARG/Matrix/08-Taxo_Filter_Plot.R
+++ Script/R_Script/M2/AV_AP_ARG/Matrix/08-Taxo_Filter_Plot.R
@@ -4,35 +4,35 @@
 # Ninon ROBIN -- ninon.robin@inserm.fr                                                  #
 # Utilite == generer les colonnes des partages inter-especes aux 6 niveaux taxonomiques #
 # et les histogrammes des nombres d occurrence de valeurs de partages a chaque niveaux  #
-# Input == sliced_all_species_taxo.tsv et taxo_species.tsv                              #
+# Input == taxo_species.tsv, uni_gene.tsv et les 6 matrices binaires                    #
 # Output == Les 6 histogrammes (en FR et EN)                                            #
 #########################################################################################
 
-#### Ouverture de sliced_all_species_taxo.tsv et taxo_species.tsv & recuperation des donnees ####
-all_species <- read_tsv('W:/ninon-species/output/Output_M2/AV_AP_ARG/Matrix/Dataframe/sliced_all_species_taxo.tsv', show_col_types = FALSE) %>% 
-  as.data.frame()
-
+#### Ouverture de taxo_species.tsv et uni_gene.tsv & recuperation des donnees ####
 species <- read_tsv('W:/ninon-species/output/Output_M2/AV_AP_ARG/Matrix/Dataframe/taxo_species.tsv', col_types = 'cccccc') %>% 
   as.data.frame() 
 
+uni_gene <- read_tsv('W:/ninon-species/output/Output_M2/AV_AP_ARG/Matrix/Dataframe/uni_gene.tsv', col_types = 'c') %>% 
+  as.data.frame()
+  
 #### Recuperation des partages au 6 niveaux taxonomiques etudies ####
 level_name <- unlist(colnames(species)) # On extrait les labels des 6 niveaux taxonomiques etudies pour pouvoir travailler a un niveau donne plus facilement
-level_shared <- as.data.frame(matrix(data=0, nrow=nrow(all_species), ncol=6))
 
 m_path_start <- "W:/ninon-species/output/Output_M2/AV_AP_ARG/Matrix/Matrice/Sliced_Matrix_"
 m_path_end <- ".tsv"
 
+level_shared <- as.data.frame(matrix(data=0, nrow=nrow(uni_gene), ncol=6))
+
 for (i in 1:6)
 {
   m_file_name <- str_glue("{m_path_start}{level_name[i]}{m_path_end}") # Le nom de fichier est definit par une variable
-  matrix <- read_tsv(m_file_name, show_col_types = FALSE)
-  rownames(matrix) <- sort(all_species$qseqid)
+  matrix <- read.csv(file = m_file_name, header = TRUE, sep = "\t")
+
+  level_share <- as.data.frame(matrix(data=0, nrow=nrow(uni_gene), ncol=1))
   
-  level_share <- as.data.frame(matrix(data=0, nrow=nrow(all_species), ncol=1))
-  
-  for (j in 1:nrow(matrix))
+  for (j in 1:ncol(matrix))
   {
-    level_share[j, ] <- sum(matrix[j,])
+    level_share[j,] <- sum(matrix[, j])
   }
   
   level_shared[, i] <- level_share
