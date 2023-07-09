831e852cb74cd3df6e2e545b8a5e46b4fcd2cbf1
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-07-09 15:37:10 +0200

    Update 01-02_alt.R

--- Script/R_Script/M2/AV_AP_ARG/Matrix/01-02_alt.R
+++ Script/R_Script/M2/AV_AP_ARG/Matrix/01-02_alt.R
@@ -8,20 +8,20 @@
 ##########################################################
 
 #### Ouverture de matrix.tsv & recuperation des donnees dans des dataframes ####
-#matrix <- read.csv('W:/ninon-species/data/matrix_ninon/100K_matrix.tsv', header = TRUE, sep = ",")
 matrix <- read.csv('W:/ninon-species/data/matrix_ninon/t_full_matrix.tsv', header = TRUE, sep = "\t")
 
 # LA TRANSPOSITION SERA FAITE EN AMONT PLUS TARD
-rownames(matrix) <- matrix[, 1]
-matrix <- matrix[, -1]
-
-matrix <- t(as.matrix(matrix))
+# rownames(matrix) <- matrix[, 1]
+# matrix <- matrix[, -1]
+# 
+# matrix <- t(as.matrix(matrix))
 # FIN TRANSPOSITION
 
 #### Suppression des fichiers vides ####
 keep_row <- rep(FALSE, nrow(matrix))
+keep_row[1] <- TRUE
 
-for (i in 1:nrow(matrix))
+for (i in 2:nrow(matrix))
 {
   keep_row[i] <- sum(matrix[i,]) > 0
 }
@@ -29,7 +29,7 @@ for (i in 1:nrow(matrix))
 matrix <- matrix[keep_row,]
 
 #### Extraction et pretraitement des noms d'especes ####
-species <- as.data.frame(rownames(matrix[, -1]))
+species <- as.data.frame(sort(rownames(matrix)))
 colnames(species) <- 'species'
 
 matrix <- as.data.frame(matrix)
@@ -39,15 +39,19 @@ matrix %>%
   arrange(species) %>%
   identity() -> matrix
 
-for (l in nrow(species)) # Inversion des 2 parties de nom d'espece pour les especes 'UNVERIFIED_ORG' pour avoir la bonne nomenclature
-{
-  if (startsWith(species[l,], 'UNV') == TRUE)
-  {
-    species[l,] <- str_replace(species[l,], pattern = "(.*)_(.*)_(.*)", replacement = "\\3\\_\\1\\_\\2.")
-  }
-}
+err <- startsWith(species[, 'species'], 'UNV')
+
+species[err, 'species'] <- str_replace(species[err, 'species'], pattern = "(.*)_(.*)_(.*)", replacement = "\\3\\_\\1\\_\\2.")
 
 matrix[, 1] <- species[, 1]
+
+species %>%
+  arrange(species) %>%
+  identity -> species
+
+matrix %>%
+  arrange(species) %>%
+  identity() -> matrix
 
 #### Enregistrement de la dataframe dans le fichier sliced_all_species_clust.tsv ####
 write.table(matrix, "W:/ninon-species/output/Output_M2/AV_AP_ARG/Matrix/Dataframe/sliced_all_species_clust.tsv", sep = '\t', row.names = FALSE, col.names = TRUE)
