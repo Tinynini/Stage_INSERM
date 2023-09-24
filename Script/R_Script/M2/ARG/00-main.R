de67553a8d633d3bf0a3b89220b07ed78fb67a7d
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-09-24 22:10:49 +0200

    Update 00-main.R

--- Script/R_Script/M2/ARG/00-main.R
+++ Script/R_Script/M2/ARG/00-main.R
@@ -3,14 +3,6 @@ library(tidytree)
 library(ape)
 library(ggtree) 
 
-#### A faire : ####
-
-# 1 : Remarques Nicolas (table excel) + Commentaires script 11
-# 2 : Switcher en bash pour pouvoir choisir si on travaille en sliced ou total (Tous sauf 01 et 03)/en anglais ou français (07 08 et 10)/avec quels reps (07 et 10) a l avance !!
-# 3 (Optionnel) : Checker l existance des inputs avant 05 06 et 09 (comment gerer ca pour plusieurs inputs ?) ??
-
-#### Main : ####
-
 if (file.exists('W:/ninon-species/output/Output_M2/ARG/Dataframe/all_species.tsv') == FALSE)
 {
   source('W:/ninon-species/script/Script_M2/ARG/01-Species_filtering.R') # /!\ Ca prend 3/4h a 2h !!
@@ -33,6 +25,4 @@ if (file.exists('W:/ninon-species/output/Output_M2/ARG/Dataframe/sliced_all_spec
   source('W:/ninon-species/script/Script_M2/ARG/08-Taxo_Filter_Plot.R')
   source('W:/ninon-species/script/Script_M2/ARG/09-Tree_Pruner.R')
   source('W:/ninon-species/script/Script_M2/ARG/10-Tree_Plot.R')
-  source('W:/ninon-species/script/Script_M2/ARG/11-New_graphs.R') 
-  # Max via all ou en tout cas plus simplement ??
 }
