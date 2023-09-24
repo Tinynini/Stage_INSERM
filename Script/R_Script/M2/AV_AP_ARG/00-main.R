f492b48babbad62c0289ea75237c4196e0faca4b
Tinynini <66923814+Tinynini@users.noreply.github.com>
2023-09-24 22:10:11 +0200

    Update 00-main.R

--- Script/R_Script/M2/AV_AP_ARG/00-main.R
+++ Script/R_Script/M2/AV_AP_ARG/00-main.R
@@ -3,14 +3,6 @@ library(tidytree)
 library(ape)
 library(ggtree) 
 
-#### A faire : ####
-
-# 1 : Remarques Nicolas (table excel) + 3 nouveaux graphs (photo antoine) + Commentaires
-# 2 : Switcher en bash pour pouvoir choisir si on travaille en sliced ou total (Tous sauf 01 et 03)/en anglais ou français (07 08 et 10)/avec quels reps (07 et 10) a l avance !!
-# 3 (Optionnel) : tester existance fichiers pour plusieurs inputs ??
-
-#### Main : ####
-
 source('W:/ninon-species/script/Script_M2/AV_AP_ARG/Matrix/01-02_alt.R') 
 source('W:/ninon-species/script/Script_M2/AV_AP_ARG/Matrix/03-Taxonomy_parser.R')
 source('W:/ninon-species/script/Script_M2/AV_AP_ARG/Matrix/04-Taxo_Join.R') 
